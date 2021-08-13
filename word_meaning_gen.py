cef15a08ce341168a5c23a803629151a82bc430f
Ishav Punia <ishav.punia@gmail.com>
2021-08-13 21:47:14 +0530

    Updated the code to add word repository creation functionality to save words to a local file

--- word_meaning_gen.py
+++ word_meaning_gen.py
@@ -5,73 +5,137 @@ import re
 from PyDictionary import PyDictionary as pd
 import streamlit as st
 import numpy as np
-
 import urllib.request
 from bs4 import BeautifulSoup
 
-pytesseract.pytesseract.tesseract_cmd = r'' #generally C:\\Program Files\\Tesseract-OCR\\tesseract.exe or your tesseract installation location
+pytesseract.pytesseract.tesseract_cmd = r'' #r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe' 
 
 #@st.cache(suppress_st_warning=True)
 def word_tiles(word):
+    dictionary = pd()
     with st.container():
-        if st.button(word):
+        if st.button(word) or "button_{}".format(word) in st.session_state:
+            st.session_state["button_{}".format(word)]=True
             if word not in st.session_state:
                #print(word)
                st.session_state[word]=str(dictionary.meaning(word))
                #print(st.session_state[word])
-            st.write(st.session_state[word])
+            st.text(st.session_state[word])
+        if st.checkbox("",key="check_{}".format(word)):
+            if "check_list" not in st.session_state:
+                st.session_state["check_list"]={}
+            st.session_state["check_list"][word]=True
+        else:
+            if "check_list" not in st.session_state:
+                st.session_state["check_list"]={}
+            st.session_state["check_list"][word]=False
+            
                     
-st.title('OpenCv2 usage')
-uploaded_file = st.file_uploader("Choose an image",["jpg","jpeg","png"])
+def create_playlist(playlist_name):
+    if playlist_name not in st.session_state:
+        st.session_state[playlist_name] = playlist_name
+        return True
+    else:
+        return False
 
-converted_text = "Nothing to convert..."
+def save_words(playlist_name,playlist_comment):
+    i=0
+    with open(r''.format(playlist_name),"a+") as fo:    #Update the upload location where the words are to be saved
+        check_list = st.session_state["check_list"]
+        fo.seek(0,0)
+        if fo.read()=="":
+            print("Empty")
+            fo.write("000000")
+            fo.seek(0,0)
+        else:
+            fo.seek(0)
+            playlist_text=fo.read()
+            print("this"+playlist_text[-6:])
+            i = int(playlist_text[-6:])
 
-text = ""
-
-if uploaded_file is not None:
-    file_bytes = np.asarray(bytearray(uploaded_file.read()),dtype=np.uint8)
-    img = cv2.imdecode(file_bytes,1)
-    text = pytesseract.image_to_string(img)
-
-#img = cv2.imread('C://Users/Rishav Punia/Desktop/circe2.jpg')
-
-word_list = []
-#print("--------------------------")
-#print(repr(text))
-text = text.replace("-\n","")
-#print("--------------------------")
-#print(repr(text))
-text_words = re.split('\n| ',text)
-for word in text_words:
-    new_word = re.sub('[^A-Za-z]+', '', word)
-    
-    if len(new_word)>=4:
-        word_list.append(new_word)
-        #print(new_word+": "+str(dict.meaning(new_word.lower()))+"\n")
+        fo.seek(0,0)
+        current_state = fo.read()
+        new_state = current_state[0:len(current_state)-6]
+        fo.seek(0)
+        fo.truncate(0)
         
+        new_state += ("\n" + "-"*25 + playlist_comment + "-"*25 + "\n")
+        for word in check_list:
+            if check_list[word]:
+                new_state += (str(i+1)+ ".> " + word+": " + st.session_state[word] + "\n")
+                i+=1
+            
+        new_state += ("-"*(50+len(playlist_comment)) + "\n")
+        fo.write(new_state)
+        fo.write(f'{i:06}')
+        fo.close()
+            
+@st.cache
+def extract_text(uploaded_file):
+    ##If file uploaded process it
+    if uploaded_file is not None:
+        file_bytes = np.asarray(bytearray(uploaded_file.read()),dtype=np.uint8)
+        img = cv2.imdecode(file_bytes,1)
+        text = pytesseract.image_to_string(img)
+        return text
+    return ""
+        
+def main():
+    st.title('OpenCv2 usage')                                                       #Title Name
+    uploaded_file = st.file_uploader("Choose an image",["jpg","jpeg","png"])        #upload widget
 
+    converted_text = "Nothing to convert..."
 
-dictionary = pd()
+    text = extract_text(uploaded_file)
 
-#print(dict.getMeanings())
+#Playlist creation/deletion form
+    playlist_form = st.form("Playlist")
+    playlist_name = playlist_form.text_input("Playlist Name")
+    playlist_comment = playlist_form.text_input("Playlist Comment")
+    if playlist_form.form_submit_button("Save"):
+        #if create_playlist(playlist_name):
+        save_words(playlist_name,playlist_comment)
+        playlist_form.text("{} saved!!!".format(playlist_name))
+        st.text(st.session_state["check_list"])
+        #else:
+            #playlist_form.text("{} already exists....".format(playlist_name))
+    
+    #img = cv2.imread('C://Users/Rishav Punia/Desktop/circe2.jpg')
 
-#word = input('Enter the word to find the meaning : ')
+    word_list = []
+    #print("--------------------------")
+    #print(repr(text))
+    text = text.replace("-\n","")
+    #print("--------------------------")
+    #print(repr(text))
+    text_words = re.split('\n| ',text)
+    for word in text_words:
+        new_word = re.sub('[^A-Za-z|*-*]+', '', word)
+        
+        if len(new_word)>=4:
+            word_list.append(new_word)
+            #print(new_word+": "+str(dict.meaning(new_word.lower()))+"\n")
 
-fo = open(r'',"r")    #Location of word filter file
-freq_words_list=fo.read().split('\n')
+    #print(dict.getMeanings())
 
-word_reduced_list = []
-#print(len(word_list))
-for word in word_list:
-    if word.lower() not in freq_words_list:
-        word_reduced_list.append(word.lower())        
+    #word = input('Enter the word to find the meaning : ')
 
-word_reduced_list = list(dict.fromkeys(word_reduced_list).keys())
-#print(len(word_reduced_list))
-for word in word_reduced_list:
-    word_tiles(word)
+    fo = open(r'',"r")         #Frequent words list to filter out some easy words
+    freq_words_list=fo.read().split('\n')
 
+    word_reduced_list = []
+    #print(len(word_list))
+    for word in word_list:
+        if word.lower() not in freq_words_list:
+            word_reduced_list.append(word.lower())        
 
+    word_reduced_list = list(dict.fromkeys(word_reduced_list).keys())
+    #print(len(word_reduced_list))
+    for word in word_reduced_list:
+        word_tiles(word)
+
+if __name__=="__main__":
+    main()
 ##
 ##
 ##for word in word_reduced_list:
